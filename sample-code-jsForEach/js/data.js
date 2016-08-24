8c5a9f74e6476bf6d451207c42cf9a56afbcb68d
GA Student <gastudent@GA-3.local>
2016-08-24 10:38:37 -0700

    add js forEach version

---
+++ sample-code-jsForEach/js/data.js
@@ -0,0 +1,129 @@
+var data = {
+  school: "General Assembly",
+  city: "San Francisco",
+  course: "Web Development Immersive",
+  course_abbreviation: "WDI",
+  cohort: 31,
+  room: 4,
+  github_schedule: "https://github.com/sf-wdi-31/schedule",
+  developers:[
+    {
+      first_name: 'Sam',
+      last_name: 'Darbonne',
+      github_username: 'samdarbonne',
+      height: 6,
+      status: {
+        loser: false,
+        married: false
+      }
+    },
+    {
+      first_name: "Alicia",
+      last_name: "Brown",
+      github_username: "cabrown91"
+    },
+    {
+      first_name: "Misha",
+      last_name: "LeClair",
+      github_username: "sfnewzgirl"
+    },
+    {
+      last_name: "Bak",
+      first_name: "Sera",
+      github_username: "serabakpak"
+    },
+    {
+      first_name: "Abbas",
+      last_name: "Baig",
+      github_username: "abbasbaigali"
+    },
+    {
+      first_name: "Andrew",
+      last_name: "Cordivari",
+      github_username: "andrewdc92"
+    },
+    {
+      first_name: "Scott",
+      last_name: "Whitman",
+      github_username: "scottwwhitman"
+    },
+    {
+      first_name: "Laura",
+      last_name: "Russell",
+      github_username: "laurakathleen"
+    },
+    {
+      first_name: "William",
+      last_name: "Namen",
+      github_username: "wnamen"
+    },
+    {
+      first_name: "Nicolas",
+      last_name: "Budig",
+      github_username: "14budig"
+    },
+    {
+      first_name: "Wolf",
+      last_name: "Reese",
+      github_username: "denzalereese"
+    },
+    {
+    first_name: "Jason",
+      last_name: "Kim",
+      github_username: "jckkim"
+    },
+    {
+      first_name: "Michael",
+      last_name: "Rogachevsky",
+      github_username: "mrogach2350"
+    },
+    {
+      first_name: "Lisa",
+      last_name: "Olson",
+      github_username: "lisaolson"
+    },
+    {
+      last_name: "Carter",
+      first_name: "Sherie",
+      github_username: "sheriecarter"
+    },
+    {
+      last_name: "Fong",
+      first_name: "Zachary",
+      github_username: "zjfong"
+    },
+    {
+      last_name: "Vernon",
+      first_name: "Ashley",
+      github_username: "ashleyvernon"
+    },
+    {
+      last_name: "Vinocur",
+      first_name: "Andrew",
+      github_username: "giants415"
+    }
+
+  ],
+  instructional_team: [
+    {
+      last_name: "Fauver",
+      first_name: "Cory",
+      github_username: "cofauver"
+    },
+    {
+      last_name: "Tsangaris",
+      first_name: "Ilias",
+      github_username: "iliastsangaris"
+    },
+    {
+      last_name: "Veenstra",
+      first_name: "Brianna",
+      github_username: "bgveenstra"
+    },
+    {
+      last_name: "Laguardia",
+      first_name: "Matt",
+      github_username: "mattlaguardia"
+    }
+  ]
+};
