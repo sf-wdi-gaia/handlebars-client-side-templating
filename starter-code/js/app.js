d1241211599472de78319ef813c4006b7c594d91
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-09-11 23:46:23 -0700

    adds starter & solution code

---
+++ starter-code/js/app.js
@@ -0,0 +1,13 @@
+console.log('Sanity Check: app.js is linked correctly!');
+
+$(document).ready(function(){
+
+  // select a template script, and pull out the handlebars and html inside it
+ 
+  // compile the handlebars template
+
+  // use the template function from handlebars to create an html string
+
+  // append html to the view
+
+});
---
+++ starter-code/js/data.js
@@ -0,0 +1,100 @@
+// some data to work with
+
+var data = {
+  school: "General Assembly",
+  city: "San Francisco",
+  course: "Web Development Immersive",
+  courseAbbreviation: "WDI",
+  developers:[
+    {
+      firstName: 'Sam',
+      lastName: 'Darbonne',
+      githubUsername: 'samdarbonne'
+    },
+    {
+      firstName: "Alicia",
+      lastName: "Brown",
+      githubUsername: "cabrown91"
+    },
+    {
+      lastName: "Misha",
+      firstName: "LeClair",
+      githubUsername: "sfnewzgirl"
+    },
+    {
+      lastName: "Bak",
+      firstName: "Sera",
+      githubUsername: "serabakpak"
+    },
+    {
+      firstName: "Abbas",
+      lastName: "Baig",
+      githubUsername: "abbasbaigali"
+    },
+    {
+      firstName: "Andrew",
+      lastName: "Cordivari",
+      githubUsername: "andrewdc92"
+    },
+    {
+      firstName: "Scott",
+      lastName: "Whitman",
+      githubUsername: "scottwwhitman"
+    },
+    {
+      firstName: "Laura",
+      lastName: "Russell",
+      githubUsername: "laurakathleen"
+    },
+    {
+      firstName: "William",
+      lastName: "Namen",
+      githubUsername: "wnamen"
+    },
+    {
+      firstName: "Nicolas",
+      lastName: "Budig",
+      githubUsername: "14budig"
+    },
+    {
+      firstName: "Wolf",
+      lastName: "Reese",
+      githubUsername: "denzalereese"
+    },
+    {
+    firstName: "Jason",
+      lastName: "Kim",
+      githubUsername: "jckkim"
+    }, 
+    {
+      firstName: "Michael",
+      lastName: "Rogachevsky",
+      githubUsername: "mrogach2350"
+    },
+    {
+      firstName: "Lisa",
+      lastName: "Olson",
+      githubUsername: "lisaolson"
+    },
+    {
+      lastName: "Carter",
+      firstName: "Sherie",
+      githubUsername: "sheriecarter"
+    },
+    {
+      lastName: "Fong",
+      firstName: "Zachary",
+      githubUsername: "zjfong"
+    },
+    {
+      lastName: "Vernon",
+      firstName: "Ashley",
+      githubUsername: "ashleyvernon"
+    },
+    {
+      lastName: "Vinocur",
+      firstName: "Andrew",
+      githubUsername: "giants415"
+    }
+  ]
+};
