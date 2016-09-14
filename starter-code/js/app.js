39e0649ff4690021fec768546fc8cbf661663ba9
brikky <brandon.kerr160@topper.wku.edu>
2016-09-14 11:45:11 -0700

    posts github user data to index.html

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -3,11 +3,11 @@ console.log('Sanity Check: app.js is linked correctly!');
 $(document).ready(function(){
 
   // select a template script, and pull out the handlebars and html inside it
- 
+ var source = $('#developer-li-template').html();
   // compile the handlebars template
-
+var template = Handlebars.compile(source);
   // use the template function from handlebars to create an html string
-
+var developerHtml = template({developers: data.developers});
   // append html to the view
-
+$('#developers-list').append(developerHtml);
 });
